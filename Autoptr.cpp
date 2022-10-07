2249534b1291f3187d11d49093f319920dcb2b2d
vrusha10 <110154094+vrusha10@users.noreply.github.com>
2022-10-07 16:33:26 +0530

    First Commit - Auto Ptr Program

---
+++ Autoptr.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include<memory>
+using namespace std;
+class A 
+{
+    public:
+    void show()
+    {
+        cout<<"Display Show"<<endl;
+    }
+};
+int main()
+{
+    auto_ptr<A>p1(new A);
+    p1->show();
+    cout<<p1.get()<<endl;
+    
+    auto_ptr<A>p2(p1);
+    p2->show();
+    cout<<p1.get()<<endl;
+    cout<<p2.get()<<endl;
+}
